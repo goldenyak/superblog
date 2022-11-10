2160e9a6fc94fd530e524876a2268c77a7631d58
Egor Yakovlev <goldenyakegor@gmail.com>
2022-11-10 21:26:31 +0300

    deploy vercel

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,15 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import * as cookieParser from 'cookie-parser';
-import { NestExpressApplication } from "@nestjs/platform-express";
+// import cookieParser from 'cookie-parser';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
 	const app = await NestFactory.create<NestExpressApplication>(AppModule);
 	app.enableCors();
 	// app.setGlobalPrefix('api');
 	app.useGlobalPipes(new ValidationPipe());
-	// app.use(cookieParser());
+	app.use(cookieParser());
 	app.set('trust proxy', 1);
 	await app.listen(process.env.PORT || 3000);
 }
