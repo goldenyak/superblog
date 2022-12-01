d8ac0adabbd0313a145feeeee3a1e8ed6a4236a8
Egor Yakovlev <goldenyakegor@gmail.com>
2022-12-02 00:19:04 +0300

    add likes for posts

--- src/posts/posts.repository.ts
+++ src/posts/posts.repository.ts
@@ -23,22 +23,23 @@ export class PostsRepository {
 			.sort({ [sortByFilter]: sortDirectionFilter })
 			.lean();
 
+		console.log(posts);
+
 		return posts.map((post) => {
-			return post;
-			// return {
-			// 	id: post.id,
-			// 	title: post.title,
-			// 	shortDescription: post.shortDescription,
-			// 	content: post.content,
-			// 	blogId: post.blogId,
-			// 	blogName: post.blogName,
-			// 	createdAt: post.createdAt,
-			// 	extendedLikesInfo: {
-			// 		likesCount: 0,
-			// 		dislikesCount: 0,
-			// 		myStatus: 'None',
-			// 	},
-			// };
+			return {
+				id: post.id,
+				title: post.title,
+				shortDescription: post.shortDescription,
+				content: post.content,
+				blogId: post.blogId,
+				blogName: post.blogName,
+				createdAt: post.createdAt,
+				extendedLikesInfo: {
+					likesCount: 0,
+					dislikesCount: 0,
+					myStatus: 'None',
+				},
+			};
 		});
 	}
 
