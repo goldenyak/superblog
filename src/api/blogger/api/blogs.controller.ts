f44ed98afb7249b238ab64a08c402cb19f205ed4
Egor Yakovlev <goldenyakegor@gmail.com>
2023-02-28 23:37:57 +0300

    fix code

--- src/api/blogger/api/blogs.controller.ts
+++ src/api/blogger/api/blogs.controller.ts
@@ -66,9 +66,9 @@ export class BlogsController {
 		if (!foundedBlog) {
 			throw new NotFoundException();
 		}
-		// if (foundedBlog.bloggerInfo.id !== req.user.id) {
-		// 	throw new ForbiddenException();
-		// }
+		if (foundedBlog.bloggerOwnerInfo.userId!== req.user.id) {
+			throw new ForbiddenException();
+		}
 		return await this.createPost.execute(dto, foundedBlog);
 	}
 
