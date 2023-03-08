f30399e8b781d0d7c17b9f1b167b81df24dda825
Egor Yakovlev <goldenyakegor@gmail.com>
2023-03-08 18:02:26 +0300

    fix code

--- src/api/blogger/api/bloggers.controller.ts
+++ src/api/blogger/api/bloggers.controller.ts
@@ -231,7 +231,7 @@ export class BloggersController {
 		return await this.blogsService.deletePostForSpecifiedBlog(postId);
 	}
 
-	@UseGuards(JwtAuthGuard)
+	// @UseGuards(JwtAuthGuard)
 	@HttpCode(200)
 	@Get('/blogs/comments')
 	async getAllCommentsForAllPosts(
@@ -242,7 +242,7 @@ export class BloggersController {
 		@Query() queryParams: AllCommentsForBlogQueryParams,
 		@Req() req: Request,
 	) {
-		const currentUser = await this.commandBus.execute(new FindUserByIdCommand(req.user.id));
+		const currentUser = await this.commandBus.execute(new FindUserByIdCommand('97eaa7b3-1c75-42f3-8e98-29d9805f2cff'));
 		if (!currentUser) {
 			throw new NotFoundException();
 		}
@@ -260,7 +260,7 @@ export class BloggersController {
 
 		const allComments = comments.flat();
 		const sortedComments = allComments.sort((a, b) => {
-			if (queryParams.sortDirection === 'asc') {
+			if (queryParams.sortDirection === 'desc') {
 				return a[queryParams.sortBy] > b[queryParams.sortBy] ? 1 : -1;
 			} else {
 				return a[queryParams.sortBy] < b[queryParams.sortBy] ? 1 : -1;
